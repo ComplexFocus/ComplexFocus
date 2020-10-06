7bc8c3fe3fd6ea271510865b8bd5df3f6b6cb9c8
Emilio Pisanty <episanty@gmail.com>
2020-10-06 19:10:23 +0200

    Added DLMF reference to implementation notes.

--- ComplexFocus.m
+++ ComplexFocus.m
@@ -40,7 +40,7 @@ $ComplexFocusVersion:="ComplexFocus v0.1, "<>$ComplexFocusTimestamp;
 
 
 (* ::Input::Initialization:: *)
-$ComplexFocusTimestamp="Tue 6 Oct 2020 17:27:46";
+$ComplexFocusTimestamp="Tue 6 Oct 2020 19:09:58";
 End[];
 
 
